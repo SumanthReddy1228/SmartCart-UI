44bfb8a06dc623301002625caad0ff90020a54e6
sumanth <sumanth0128@gmail.com>
2026-06-02 18:13:00 +0530

    Added Header and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import "./App.css";
-
+import Header from "./Components/Header";
+import Footer from "./Components/Footer/Footer";
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,55 @@
+import {
+  faBagShopping,
+  faCartShopping,
+  faShoppingBasket,
+  faStore,
+  faTags,
+} from "@fortawesome/free-solid-svg-icons";
+import { faShoppingBag } from "@fortawesome/free-solid-svg-icons/faShoppingBag";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <div className="container">
+          <a href="/">
+            <FontAwesomeIcon icon={faStore} className="fa-icon" />
+            <span>SmartCart</span>
+          </a>
+          <nav className="nav">
+            <ul>
+              <li>
+                <a href="/home" className="nav-link">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="/about" className="nav-link">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="/contact" className="nav-link">
+                  Contact
+                </a>
+              </li>
+              <li>
+                <a href="/login" className="nav-link">
+                  Login
+                </a>
+              </li>
+              <li>
+                <a href="cart" className="nav-link">
+                  <FontAwesomeIcon icon={faShoppingBasket} />
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
